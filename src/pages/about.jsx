64f7aeef160bed2a9db8293ade666b771ddee26f
Ali <13923942+princetechn0@users.noreply.github.com>
2022-11-29 16:02:54 -0800

    uci

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,28 +1,12 @@
 import React, { Component } from "react";
-import Card from "react-bootstrap/Card";
 import "../stylesheets/about.css";
-import Carousel from "react-bootstrap/Carousel";
 
 class About extends Component {
-  componentDidMount() {
-    // document.body.classList.add("gradient-custom");
-  }
-
-  state = {
-    quotes: [""],
-  };
-
   render() {
     return (
       <div className="container">
         <div className="about-text py-5 text-center text-white">
-          <span>
-            {" "}
-            For the Culture is an online “Black book” that encapsulates all
-            things Black Culture as it relates to cuisine, music, church, hair,
-            organizations, social spaces, etc., which are seen as fundamental
-            iconography in the lexicon of Blackness.
-          </span>
+          <h2 className="py-5"> Made by UCI students </h2>
         </div>
       </div>
     );
