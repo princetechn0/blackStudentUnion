8d83726224116ece33a9453a29be597a0d2cfe37
Ali <13923942+princetechn0@users.noreply.github.com>
2023-01-21 00:42:40 -0800

    modded about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import "../stylesheets/about.css";
+import { Button } from "react-bootstrap";
 
 class About extends Component {
   render() {
     return (
       <div className="container">
-        <div className="about-text py-5 text-center text-white">
-          <h2 className="py-5"> Made by UCI students </h2>
+        <div className="about-text">
+          <h2> Made by UCI students, </h2>
+          <h3> for UCI students. </h3>
+
+          <footer>
+            <Button
+              variant="light"
+              href="http://bsu-uci.weebly.com/"
+              target="_blank"
+            >
+              Learn More
+            </Button>
+          </footer>
         </div>
       </div>
     );
